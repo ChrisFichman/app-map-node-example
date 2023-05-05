897bd80dabfb46d21d852313ba2a36b020c90748
Christopher Fichman <chris.fichman@gmail.com>
2023-05-05 13:55:55 -0400

    fix - regenerated initial angular project

--- web-portal/src/app/app.component.spec.ts
+++ web-portal/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'url-shortener'`, () => {
+  it(`should have as title 'web-portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('url-shortener');
+    expect(app.title).toEqual('web-portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('url-shortener app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('web-portal app is running!');
   });
 });
